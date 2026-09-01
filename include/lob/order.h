c64052652af045a4493d2a4e74afe82ce560d283
Benjamin Tam <benjamint1205@gmail.com>
2026-09-01 09:54:37 -0400

    feat(order): add Order struct and Side enum class

---
+++ include/lob/order.h
@@ -0,0 +1,25 @@
+#ifndef LOB_INCLUDE_LOB_ORDER_H_
+#define LOB_INCLUDE_LOB_ORDER_H_
+
+#include <cstdint>
+
+namespace lob {
+
+enum class Side : uint8_t {
+  kBid,
+  kAsk
+};
+
+struct Order {
+  std::uint64_t id = 0;
+  std::uint64_t price_ticks = 0;
+  std::uint32_t remaining_quantity = 0;
+  Side side = Side::kBid;
+
+  Order* next = nullptr;
+  Order* prev = nullptr;
+};
+
+}  // namespace lob
+
+#endif  // LOB_INCLUDE_LOB_ORDER_H_
